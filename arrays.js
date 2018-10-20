b1312ac34e4d9aa10570055fc0ebade3ff16a39b
Nora Vogt <noraevogt@gmail.com>
2018-10-20 16:42:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,20 @@ var chocolateBars = [
 
 function addElementToBeginningOfArray(array, element) {
   const array = ['1']
-  const add = ['foo', ...one]
-  return add
+  const element = ['foo', ...array]
+  return element
 };
+
+describe('addElementToBeginningOfArray(array, element)', () => {
+  it('adds an element to the beginning of an array', () => {
+    expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+  })
+
+  it('does not alter the original array', () => {
+    const array = [1]
+
+    addElementToBeginningOfArray(array, 'foo')
+
+    expect(array).to.eql([1])
+  })
+})
