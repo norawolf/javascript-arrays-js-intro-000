f77d205d1b4262b87dd09a2f2d9a88eced8741d8
Nora Vogt <noraevogt@gmail.com>
2018-10-20 16:32:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,11 @@ var chocolateBars = [
   'kitkat',
   'skittles',
 ];
+
+function addElementToBeginningOfArray() {
+  var dogs = ['pomeranian', 'husky', 'golden retriever']
+  dogs = ['german shepherd', ...dogs]
+  return dogs
+}
+
+function destructivelyAddElementToBeginningOfArray
