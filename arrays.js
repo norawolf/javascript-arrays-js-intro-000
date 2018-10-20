a450ac4b0051d8cb761cca05c37cbdf67ddd8199
Nora Vogt <noraevogt@gmail.com>
2018-10-20 16:43:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,17 +10,3 @@ function addElementToBeginningOfArray(array, element) {
   const element = ['foo', ...array]
   return element
 };
-
-describe('addElementToBeginningOfArray(array, element)', () => {
-  it('adds an element to the beginning of an array', () => {
-    expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
-  })
-
-  it('does not alter the original array', () => {
-    const array = [1]
-
-    addElementToBeginningOfArray(array, 'foo')
-
-    expect(array).to.eql([1])
-  })
-})
